111ea24263c74546ae13df7f8ab61a8beeccbb08
nackcimelesperance <107566158+nackcimelesperance@users.noreply.github.com>
2022-06-17 14:36:11 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -10,23 +10,28 @@ from fastapi import FastAPI
 import numpy as np
 import pickle
 import pandas as pd
-from processing_functions import add_variable
+#from processing_functions import add_variable
 
 # 2. Create the app object
 app = FastAPI()
 
-data_train = pd.read_csv('application_train.csv')
+#data_train = pd.read_csv('application_train.csv')
 
 #import model and data treatment process
-pickle_in = open("pipeline_bank.pkl","rb")
-pipeline_process=pickle.load(pickle_in)
+#pickle_in = open("pipeline_bank.pkl","rb")
+#pipeline_process=pickle.load(pickle_in)
 
-add_variable(data_train)
+#add_variable(data_train)
 
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+"""
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted Bank Note with the confidence
 @app.get('/predict')
 def predict_note(id):
+
     col_index = len(data_train.columns)
     row_index =np.where(data_train['SK_ID_CURR'] == id)
     column_value_id = data_train.iloc[row_index[0][0], 1:col_index]
@@ -39,5 +44,5 @@ def predict_note(id):
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-    
+"""    
 #uvicorn app:app --reload
