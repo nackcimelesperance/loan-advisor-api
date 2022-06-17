86d58a18090aea284d707ccdcd8d616039d1f4b3
nackcimelesperance <107566158+nackcimelesperance@users.noreply.github.com>
2022-06-17 15:05:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def index():
 
     # For debugging
     print(f"Received: {name}")
-    return {'message': 'Hello, World'}
+    #return {'message': name}
 """
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted Bank Note with the confidence
@@ -46,7 +46,8 @@ def predict_note():
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
+"""    
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-"""    
+
 #uvicorn app:app --reload
