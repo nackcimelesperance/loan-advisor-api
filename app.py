a1a7b3290f3931a52d63e131edc4e6ade16949b2
nackcimelesperance <107566158+nackcimelesperance@users.noreply.github.com>
2022-06-23 20:10:51 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -11,28 +11,59 @@ import numpy as np
 import pickle
 import pandas as pd
 from processing_functions import add_variable, get_client
+from pydantic import BaseModel
 
+#import sklearn
+#from lightgbm import LGBMClassifier
 # 2. Create the app object /  Initialize an instance of FastAPI
 app = FastAPI()
 
 
-#pickle_in = open("pipeline_bank.pkl","rb")
-#pipeline_process=pickle.load(pickle_in)
+pickle_in = open("pipeline_bank.pkl","rb")
+pipeline_process=pickle.load(pickle_in)
 
 data_train = pd.read_csv('application_train.csv')
 
-#add_variable(data_train)
+add_variable(data_train)
 
 #get_client(id,data_train)
 @app.get('/')
 def index():
     col_index = len(data_train.columns)
-    return {'message': col_index}
+    return {'Welcome with': col_index}
 
-@app.get('/{name}')
-def get_name(name: str):
-    return {'Welcome ': f'{name}'}
+#@app.get('/{name}')
+#def get_name(name : str):
+#    return {'Welcome ': f'{name}'}
 
+#Class which describes a single id
+class Item(BaseModel):
+    id: float
+
+@app.post('/predict')
+def predict_bank(item : Item):
+    data = item.dict()
+    #col_index = len(data_train.columns)
+    row_index = data_train['SK_ID_CURR'].tolist().index(data['id'])
+    column_value_id = data_train.iloc[row_index][2:]
+    #row_index =np.where(data_train['SK_ID_CURR'] == data['id'])
+    #column_value_id = data_train.iloc[row_index[0][0], 1:col_index]
+    df_column_value_id = column_value_id.to_frame().T
+    prediction=pipeline_process.predict_proba(df_column_value_id)[:,1]
+    id_score = str(prediction[0])
+    print(id_score)
+    return id_score 
+
+#@app.get('/predict/{id}')
+#def predict_note(id):
+
+    col_index = len(data_train.columns)
+    #row_index =np.where(data_train['SK_ID_CURR'] == id)
+    #column_value_id = data_train.iloc[row_index[0][0], 1:col_index]
+    #df_column_value_id = column_value_id.to_frame().T
+    #prediction=pipeline_process.predict_proba(df_column_value_id)[:,1]
+    #print(prediction)
+    #return prediction
 """
 @app.get('/')
 def get_name():
